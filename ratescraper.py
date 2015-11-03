1253842f101c60b2ea41175b55fc460e0424505b
Karl <kturrell@hotmail.com>
2015-11-03 22:01:34 +0000

    Create calling cost skeleton function
    Add function to get the calling cost, module constant for URL to scrape,
    and placeholder functionality.

--- ratescraper.py
+++ ratescraper.py
@@ -1,6 +1,21 @@
 from selenium import webdriver
 
-if __name__ == "__main__":
+SCRAPE_URL = ("http://international.o2.co.uk/internationaltariffs/"
+   "calling_abroad_from_uk")
+
+def get_calling_cost(country):
+   """
+   Get the per minute calling cost to a landline in a specific country 
+   on an O2 monthly contract.
+
+   Keyword arguments:
+   country -- the country name to query
+
+   Returns: string
+   """
    driver = webdriver.Chrome()
-   driver.get("http://www.cheese.com")
-   print(driver.title)
+   driver.get(SCRAPE_URL)
+   return driver.title
+
+if __name__ == "__main__":
+   print(get_calling_cost("new zealand"))
