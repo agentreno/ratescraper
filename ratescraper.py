968aedaad694c76f4351bb96ac3deb2144b0db04
Karl <kturrell@hotmail.com>
2015-11-04 08:43:37 +0000

    Add basic exception handling
    Very broad exception handling, catching all WebDriver exceptions and one
    specific exception likely to occur when an incorrect country is provided.
    More granular exception handling should be added in the future.

--- ratescraper.py
+++ ratescraper.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
+from selenium.common.exceptions import (
+   WebDriverException, NoSuchElementException)
 
 SCRAPE_URL = ("http://international.o2.co.uk/internationaltariffs/"
    "calling_abroad_from_uk")
@@ -17,25 +19,35 @@ def get_calling_cost(country):
 
    Returns: string representing the cost
    """
-   driver = webdriver.Chrome()
-   driver.get(SCRAPE_URL)
+   try:
+      driver = webdriver.Chrome()
+      driver.get(SCRAPE_URL)
+   
+      # Search on country in search form
+      search_element = driver.find_element_by_id("countryName")
+      search_element.click()
+      search_element.send_keys(country)
+      search_element.send_keys(Keys.ENTER)
+   
+      # Wait for element to be present, then open the pay monthly tab
+      try:
+         driver.implicitly_wait(5)
+         tab_element = driver.find_element_by_id("paymonthly")
+         tab_element.click()
+      except NoSuchElementException:
+         raise ValueError("Country not found or scrape page changed")
+   
+      # On the correct page, use xpath to find the calling cost - found in
+      # a specific table on the following table cell after content 'Landline'
+      cost_element = driver.find_element_by_xpath(
+         "//table[@id='standardRatesTable']//td[.='Landline']/following-sibling::td")
+      return cost_element.text
 
-   # Search on country in search form
-   search_element = driver.find_element_by_id("countryName")
-   search_element.click()
-   search_element.send_keys(country)
-   search_element.send_keys(Keys.ENTER)
-
-   # Wait for element to be present, then open the pay monthly tab
-   driver.implicitly_wait(10)
-   tab_element = driver.find_element_by_id("paymonthly")
-   tab_element.click()
-
-   # On the correct page, use xpath to find the calling cost - found in
-   # a specific table on the following table cell after content 'Landline'
-   cost_element = driver.find_element_by_xpath(
-      "//table[@id='standardRatesTable']//td[.='Landline']/following-sibling::td")
-   return cost_element.text
+   except WebDriverException:
+      raise RuntimeError("Error using Chrome WebDriver or scrape page changed")
 
 if __name__ == "__main__":
-   print(get_calling_cost("new zealand"))
+   try:
+      print(get_calling_cost("new zealand"))
+   except (ValueError, RuntimeError) as e:
+      print(e.args)
